2cbaafc363e2dce8766f1a6885ff498e9bff9935
Euiyoung Hwang <euiyoung.hwang@gxo.com>
2026-04-27 14:15:20 -0500

    Update

--- tests/test_elasticsearch.py
+++ tests/test_elasticsearch.py
@@ -7,3 +7,7 @@ import json
 def test_elasticsearch(mock_es_client):
     # ES 7 will be on 9200 and setup basic index with some datas into local elasticsearch cluster
     assert mock_es_client is not None
+
+    # Verify the cluster name
+    response = mock_es_client.info()
+    assert response['cluster_name'] == 'docker-elasticsearch'
